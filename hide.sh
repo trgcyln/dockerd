979027f1d34d68c55ab136b24c2c9707bf440d41
Turgay Ceylan <turgay@turgayceylan.com>
2022-02-26 19:13:05 +0300

    Add files via upload

---
+++ hide.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo '[data-name="Docker.oexe"] { display:none !important; }' >> /usr/www/css/ctools.css
+echo 'li#App_docker { display: none !important; }' >> /usr/www/css/ctools.css
+echo 'li.act {display: none !important;}' >> /usr/www/css/ctools.css
